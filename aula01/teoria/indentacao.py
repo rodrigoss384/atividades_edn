9c8c54220a64ec77fa0542883f62a0fb9162cd66
rodrigoss384 <rodrigoss384@gmail.com>
2025-05-12 21:18:08 -0300

    Atividades EdN Atualizadas

---
+++ aula01/teoria/indentacao.py
@@ -0,0 +1,8 @@
+if True:
+    print("Primeiro Nível")
+    if True:
+        print("Segundo Nível")
+        print("Ainda no Segundo Nível")
+    print("Voltei para o Primeiro Nível")
+print("Fora de tudo")
+# O código acima é um exemplo de indentação em Python.
---
+++ aula01/teoria/variaveis.py
@@ -0,0 +1,12 @@
+nome = "Marcos"
+_nome = "Marcos"
+nome = 2025
+sobreome_marcos = "Teixeira"
+Nome = "Marcos2"
+
+idade = 30
+altura = 1.8
+
+print(nome)
+print(type(idade))
+print(type(altura))
